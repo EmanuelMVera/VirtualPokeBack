83d3a2b7d62427f27b5b1631670dad0a931428a5
Emanuel <emanuel_vera@live.com.ar>
2022-08-02 07:17:44 -0300

    modified index.js, agruegue una variable de entorno PORT

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn, Types } = require("./src/db.js");
 const axios = require("axios");
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
@@ -33,7 +15,5 @@ conn.sync({ force: true }).then(async () => {
   });
   await Types.bulkCreate(typesData);
 
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
-  });
+  server.listen(PORT, () => console.log(`Listen on port ${PORT}`));
 });
