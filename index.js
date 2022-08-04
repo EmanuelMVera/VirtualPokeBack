044c55f03e9eb44604d8bc33b41801bb29dc4f45
Emanuel <emanuel_vera@live.com.ar>
2022-08-04 08:10:58 -0300

    sync true

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   //Precarga de tipos
   const types = await axios.get(`https://pokeapi.co/api/v2/type`);
 
